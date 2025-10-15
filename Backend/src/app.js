d3387a371a2768b99dd1b2db6f4c5bfc38a2d690
Mashood-Zah1d <itsmashood18@gmail.com>
2025-10-15 07:08:58 -0700

    Login routes Completed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,9 +8,9 @@ app.use(urlencoded({extended:true,limit:"16kb"}))
 app.use(express.json({limit:"16kb"}))
 app.use(cookieParser())
 app.use(cors({
-    origin:process.env.Origin,
-    credentials:true
-}))
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use("/api/v1/admin",adminRouter)
 
